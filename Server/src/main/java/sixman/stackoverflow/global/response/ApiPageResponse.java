d1d94ba7a58c758bf236adeb8144bb3241734ecb
kimhobeen <sksjsksh32@naver.com>
2023-08-06 23:20:06 +0900

    feat : 공통 ApiReponse 추가

--- Server/src/main/java/sixman/stackoverflow/global/response/ApiPageResponse.java
+++ Server/src/main/java/sixman/stackoverflow/global/response/ApiPageResponse.java
@@ -1,2 +1,39 @@
-package sixman.stackoverflow.global.response;public class ApiPageResponse {
+package sixman.stackoverflow.global.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Getter(value = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiPageResponse<T> {
+
+    private List<T> data;
+    private PageInfo pageInfo;
+    private int code;
+    private String status;
+    private String message;
+
+    public static <S, T extends Page<S>> ApiPageResponse<S> ok(T data) {
+        return ApiPageResponse.of(data, HttpStatus.OK);
+    }
+
+    public static <S, T extends Page<S>> ApiPageResponse<S> of(T data, HttpStatus httpStatus) {
+        return ApiPageResponse.of(data, httpStatus, httpStatus.getReasonPhrase());
+    }
+
+    public static <S, T extends Page<S>> ApiPageResponse<S> of(T data, HttpStatus httpStatus, String message) {
+        return new ApiPageResponse<>(
+                data.getContent(),
+                PageInfo.of(data),
+                httpStatus.value(),
+                httpStatus.name(),
+                message);
+    }
 }
--- Server/src/main/java/sixman/stackoverflow/global/response/PageInfo.java
+++ Server/src/main/java/sixman/stackoverflow/global/response/PageInfo.java
@@ -1,2 +1,34 @@
-package sixman.stackoverflow.global.response;public class PageInfo {
+package sixman.stackoverflow.global.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+@Getter(value = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PageInfo {
+
+    private int page;
+    private int size;
+    private int totalPage;
+    private int totalSize;
+    private boolean first;
+    private boolean last;
+    private boolean hasNext;
+    private boolean hasPrevious;
+
+    public static PageInfo of(Page<?> page){
+        return new PageInfo(
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalPages(),
+                (int) page.getTotalElements(),
+                page.isFirst(),
+                page.isLast(),
+                page.hasNext(),
+                page.hasPrevious()
+        );
+    }
 }
