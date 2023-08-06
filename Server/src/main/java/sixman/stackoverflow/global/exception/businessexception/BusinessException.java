a9db0318a7afebd85cc9182e7dfe3e7e93396dd6
kimhobeen <sksjsksh32@naver.com>
2023-08-06 23:19:41 +0900

    feat: 공통 비즈니스 exception 추가

--- Server/src/main/java/sixman/stackoverflow/global/exception/businessexception/BusinessException.java
+++ Server/src/main/java/sixman/stackoverflow/global/exception/businessexception/BusinessException.java
@@ -1,2 +1,18 @@
-package sixman.stackoverflow.global.exception.businessexception;public class BusinessException {
+package sixman.stackoverflow.global.exception.businessexception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public abstract class BusinessException extends RuntimeException {
+
+    private final String errorCode;
+    private final HttpStatus httpStatus;
+
+    protected BusinessException(String errorCode, HttpStatus httpStatus, String message) {
+        super(message);
+        this.errorCode = errorCode;
+        this.httpStatus = httpStatus;
+    }
+
 }
