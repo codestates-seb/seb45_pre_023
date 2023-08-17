9d872c305c0558317fd74d694a1da6785f8162c9
kimhobeen <sksjsksh32@naver.com>
2023-08-17 09:59:13 +0900

    ♻ : security 활성화 #34

--- Server/src/main/java/sixman/stackoverflow/auth/SecurityConfig.java
+++ Server/src/main/java/sixman/stackoverflow/auth/SecurityConfig.java
@@ -55,9 +55,8 @@ public class SecurityConfig {
                 .accessDeniedHandler(new MemberAccessDeniedHandler())
                 .authenticationEntryPoint(new MemberAuthenticationEntryPoint());
 
-//        http.authorizeRequests(getAuthorizeRequestsCustomizer());
-        http.authorizeRequests()
-                .anyRequest().permitAll();
+        http.authorizeRequests(getAuthorizeRequestsCustomizer());
+
 
         return http.build();
     }
