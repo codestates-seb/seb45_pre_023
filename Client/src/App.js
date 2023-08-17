6cce004d702e03629302a5401cd80210ccfb8d5a
toystory23 <ghlduq@naver.com>
2023-08-17 16:17:21 +0900

    충돌사항 수정

--- Client/src/App.js
+++ Client/src/App.js
@@ -11,23 +11,8 @@ import MemberProfile from './pages/Member/Profile';
 import MemberEdit from './pages/Member/Settings/Edit';
 import MemberDelete from './pages/Member/Settings/Delete';
 import MemberMain from './pages/Member/memberMain';
-<<<<<<< HEAD
-import { useSelector } from 'react-redux';
-
-=======
-import LeftSidebar from './components/SideBar/LeftSidebar';
-import RightSidebar from './components/SideBar/RightSidebar';
->>>>>>> c0be298c182c80ddd02818bb3d94bb6c0ca81462
-import { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-
-<<<<<<< HEAD
-function App() {
-  const [isLogin, setIsLogin] = useState(false);
-  const [accessToken, setAccessToken] = useState('');
-
-  const provider = useSelector((state) => state.oauth.value);
-=======
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 import axios from 'axios';
 import { oauthtoken } from './redux/createSlice/OAuthSlice';
@@ -35,7 +20,6 @@ import { oauthtoken } from './redux/createSlice/OAuthSlice';
 function App() {
   const dispatch = useDispatch();
   const provider = useSelector((state) => state.oauth.provider);
->>>>>>> c0be298c182c80ddd02818bb3d94bb6c0ca81462
 
   const getAccessToken = async (authorizationCode) => {
     return axios
@@ -61,15 +45,6 @@ function App() {
 
   return (
     <BrowserRouter>
-<<<<<<< HEAD
-      <Header isLogin={isLogin} />
-      <Routes>
-        <Route path={RouteConst.SignUp} element={<SignUp />} />
-        <Route path={RouteConst.Login} element={<Login />} />
-        <Route path={RouteConst.Ask} element={<Ask />} />
-        <Route path={RouteConst.Main} element={<Main />} />
-        <Route path={RouteConst.Question} element={<Question />} />
-=======
       <Header />
       {/* <div className="flex ml-40 h-[80rem]"> */}
       {/* <LeftSidebar /> */}
@@ -79,18 +54,13 @@ function App() {
         <Route path={RouteConst.Main} element={<Main />} />
         <Route path={RouteConst.Question} element={<Question />} />
         <Route path={RouteConst.Ask} element={<Ask />} />
->>>>>>> c0be298c182c80ddd02818bb3d94bb6c0ca81462
         <Route path={RouteConst.memberMain} element={<MemberMain />} />
         <Route path={RouteConst.memberProfile} element={<MemberProfile />} />
         <Route path={RouteConst.memberEdit} element={<MemberEdit />} />
         <Route path={RouteConst.memberDelete} element={<MemberDelete />} />
       </Routes>
-<<<<<<< HEAD
-
-=======
       {/* <RightSidebar /> */}
       {/* </div> */}
->>>>>>> c0be298c182c80ddd02818bb3d94bb6c0ca81462
       <Footer />
     </BrowserRouter>
   );
