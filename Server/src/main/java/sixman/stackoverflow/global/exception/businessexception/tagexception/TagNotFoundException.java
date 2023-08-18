199d7fa5eb8cf688b713e663d76fd07f5c53cf30
kimhobeen <sksjsksh32@naver.com>
2023-08-19 01:11:20 +0900

    ✨  feat : batchsize추가 : 100#34

--- Server/src/main/java/sixman/stackoverflow/global/exception/businessexception/tagexception/TagNotFoundException.java
+++ Server/src/main/java/sixman/stackoverflow/global/exception/businessexception/tagexception/TagNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public class TagNotFoundException extends TagException {
 
-    public static final String MESSAGE = "태그을 찾을 수 없습니다.";
+    public static final String MESSAGE = "태그를 찾을 수 없습니다.";
     public static final String CODE = "TAG-404";
 
     public TagNotFoundException() {
