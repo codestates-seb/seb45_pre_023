9e5449e2e22cf1f3b956cd2909c771baab1ce8b3
kimhobeen <sksjsksh32@naver.com>
2023-08-19 15:37:19 +0900

    ♻ : reqeusttypemismatch 405 에서 400 으로 변경 #34

--- Server/src/main/java/sixman/stackoverflow/global/exception/businessexception/requestexception/RequestTypeMismatchException.java
+++ Server/src/main/java/sixman/stackoverflow/global/exception/businessexception/requestexception/RequestTypeMismatchException.java
@@ -5,10 +5,10 @@ import org.springframework.http.HttpStatus;
 public class RequestTypeMismatchException extends RequestException {
 
     public static final String MESSAGE = "요청 값의 타입이 잘못되었습니다.";
-    public static final String CODE = "REQUEST-405";
+    public static final String CODE = "REQUEST-400";
 
     public RequestTypeMismatchException() {
-        super(CODE, HttpStatus.METHOD_NOT_ALLOWED, MESSAGE);
+        super(CODE, HttpStatus.BAD_REQUEST, MESSAGE);
     }
 
     public RequestTypeMismatchException(String failedValue) {
